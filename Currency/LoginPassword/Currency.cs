6192174dab681f26dd486f393b405e4de1370667
Александр <ussov39@gmail.com>
2019-09-01 13:18:18 +0300

    Создал приложение с получением Json файла и его парсингом

---
+++ Currency/LoginPassword/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace LoginPassword
+{
+    class Currency
+    {
+        
+        [JsonProperty("USD_PHP")]
+        public double course { get; set; }
+    }
+}
---
+++ Currency/LoginPassword/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+namespace LoginPassword
+{
+    public partial class Form1 : Form
+    {
+        public HttpClient http = new HttpClient();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            HttpResponseMessage response = await http.GetAsync(new Uri("https://free.currconv.com/api/v7/convert?q=USD_PHP&compact=ultra&apiKey=a0115f69bd71c7635a86"));
+            var result = await response.Content.ReadAsStringAsync();
+            Currency a = new Currency();
+            a = JsonConvert.DeserializeObject<Currency>(result);
+            label2.Text = a.course.ToString();
+        }
+    }
+}
